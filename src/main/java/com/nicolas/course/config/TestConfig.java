9e723a95e6be3d412934dd1b71f075449f4fbd20
Nicolas Hoffmann <nicolasfhh1992@hotmail.com>
2022-09-23 16:37:35 -0300

    Payment, one-to-one association

--- src/main/java/com/nicolas/course/config/TestConfig.java
+++ src/main/java/com/nicolas/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.nicolas.course.entities.Category;
 import com.nicolas.course.entities.Order;
 import com.nicolas.course.entities.OrderItem;
+import com.nicolas.course.entities.Payment;
 import com.nicolas.course.entities.Product;
 import com.nicolas.course.entities.User;
 import com.nicolas.course.entities.enums.OrderStatus;
@@ -80,5 +81,10 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 }
